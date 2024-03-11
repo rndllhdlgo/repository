0c6419fbc4ede9dde9a1eb03dffdb49c8c7b7783
lancenacabuan <lancenacabuan@outlook.com>
2024-03-11 14:00:00 +0800

    Websocket Modified

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
+// import 'bootstrap';
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,10 +6,10 @@ import 'bootstrap';
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+// import axios from 'axios';
+// window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -26,9 +26,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
+    wsHost: 'ws.apsoft.com.ph',
+    wsPort: 6002,
+    wssPort: 6002,
     encrypted: true,
     // wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: true, //(import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
